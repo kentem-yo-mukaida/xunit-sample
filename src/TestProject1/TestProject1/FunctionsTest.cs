2ca077b6afe5f5149dba99f3f2f976f8e43f2dd9
yo-mukaida <yo-mukaida@kentem.co.jp>
2022-12-01 13:28:54 +0900

    失敗するテスト

--- src/TestProject1/TestProject1/FunctionsTest.cs
+++ src/TestProject1/TestProject1/FunctionsTest.cs
@@ -17,6 +17,8 @@ namespace TestProject1
             Assert.Equal(1, _functions.Sum(1));
             Assert.Equal(3, _functions.Sum(1, 2));
             Assert.Equal(6, _functions.Sum(1, 2, 3));
+
+            Assert.Equal(6, _functions.Sum(1, 2, 3, 4));
         }
     }
 }
