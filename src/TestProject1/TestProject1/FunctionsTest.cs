0c5f89c13c1723c155e7b125c3072ed5327a0e57
yo-mukaida <yo-mukaida@kentem.co.jp>
2022-12-01 13:14:51 +0900

    テストコードを実装

---
+++ src/TestProject1/TestProject1/FunctionsTest.cs
@@ -0,0 +1,22 @@
+﻿using TestProject1.Models;
+
+namespace TestProject1
+{
+    public class FunctionsTest
+    {
+        private readonly Functions _functions;
+
+        public FunctionsTest()
+        {
+            _functions = new Functions();
+        }
+
+        [Fact]
+        public void Sum()
+        {
+            Assert.Equal(1, _functions.Sum(1));
+            Assert.Equal(3, _functions.Sum(1, 2));
+            Assert.Equal(6, _functions.Sum(1, 2, 3));
+        }
+    }
+}
